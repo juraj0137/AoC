72d60f13b2738b2398dba7ef9a1c706a10600967
Juraj Kubala <juraj.alabuk@gmail.com>
2024-12-07 20:22:11 +0100

    Adjust scaffolding script

--- scripts/scaffold.js
+++ scripts/scaffold.js
@@ -17,28 +17,39 @@ fs.writeFileSync(
     path.join(dayDir, 'solution.ts'),
     `export const solvePart1 = (input: string): any => {
     return null;
-  };
+};
 
   export const solvePart2 = (input: string): any => {
     return null;
-  };`,
+};`,
 );
 
 fs.writeFileSync(
     path.join(dayDir, 'solution.test.ts'),
     `import { solvePart1, solvePart2 } from './solution';
+import { readFileSync } from 'fs';
 
-  describe('Day ${day} Solution (${year})', () => {
+const file = readFileSync(\`\${__dirname}/input.txt\`, 'utf-8');
+
+describe('Day ${day} Solution (${year})', () => {
     const input = \`\`; 
 
     it('should solve Part 1', () => {
-      expect(solvePart1(input)).toEqual(null); 
+        expect(solvePart1(input)).toEqual(null); 
+    });
+    
+    it.skip('should solve Part 1 with real data', () => {
+        expect(solvePart1(file)).toEqual(null); 
     });
 
     it('should solve Part 2', () => {
-      expect(solvePart2(input)).toEqual(null); 
+        expect(solvePart2(input)).toEqual(null); 
     });
-  });`,
+    
+    it.skip('should solve Part 2 with real data', () => {
+        expect(solvePart2(file)).toEqual(null); 
+    });
+});`,
 );
 
 console.log(`Scaffolded Year ${year}, Day ${day} at ${dayDir}`);
